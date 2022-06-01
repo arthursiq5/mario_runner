ff6b1e9c2e8dc53aa2cbc14aead4ee7d60034c8d
arthursiq5 <arthur.siq5@gmail.com>
2022-05-31 21:29:26 -0300

    melhorada função de pulo

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 const mario = document.querySelector('.mario')
+var jumping = false
 
 const jump = () => {
+    if (jumping == true) return
     mario.classList.add('jump')
+    jumping = true
     setTimeout(() => {
         mario.classList.remove('jump')
+        jumping = false
     }, 500)
 }
 
