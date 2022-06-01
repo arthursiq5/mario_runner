51fc3ac43d29b06c7f6e4fdff7aeaf1b1a11e311
arthursiq5 <arthur.siq5@gmail.com>
2022-05-31 23:48:57 -0300

    Adicionada função de restart

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
+const board = document.querySelector('.game-board')
 const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
+const restartBtn = document.getElementById('restart')
 var jumping = false
 var fimDeJogo = false
+
+const restart = () => {
+    fimDeJogo = false
+    pipe.style.left = 'initial'
+    pipe.style.right = '-80px'
+    mario.style.bottom = '0'
+    mario.src = './images/mario.gif'
+    board.classList.remove('game-over')
+    jumping = false
+}
 
 const jump = () => {
     if (jumping && !fimDeJogo) return
@@ -14,23 +26,22 @@ const jump = () => {
 }
 
 const loop = setInterval(() => {
+    if (fimDeJogo) return;
     const pipePosition = pipe.offsetLeft
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
 
-    if (pipePosition <= 120 && pipePosition >= -10 && marioPosition < 80) {
-        pipe.style.animation = 'none'
+    if (pipePosition <= 120 && pipePosition >= 0 && marioPosition < 80) {
+        board.classList.add('game-over')
+
         pipe.style.left = `${pipePosition}px`
 
-        mario.style.animation = 'none'
         mario.style.bottom = `${marioPosition}px`
         mario.src = './images/game-over.png'
-        mario.style.width = '75px'
-        mario.style.marginLeft = '50px'
 
         fimDeJogo = true
-
-        clearInterval(loop)
     }
 }, 10)
 
 document.addEventListener('keydown', jump)
+restartBtn.addEventListener('click', restart)
+restart()
